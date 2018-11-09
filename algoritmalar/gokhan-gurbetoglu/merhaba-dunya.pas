bdb78a60fa3dc2e19a09b6a901019efbb9d90569
Gökhan Gurbetoğlu <ggurbet@gmail.com>
2018-11-09 10:42:57 +0300

    Pascal programlama dilinde Merhaba Dünya programı

---
+++ algoritmalar/gokhan-gurbetoglu/merhaba-dunya.pas
@@ -0,0 +1,9 @@
+// Gökhan Gurbetoğlu
+// GitHub: https://github.com/ggurbet
+// Twitter: https://twitter.com/ggurbet
+// Programlama Dili: Pascal
+
+program Hello;
+begin
+  writeln ('Merhaba Dünya!');
+end.

58e86988bad8d55bda3b762dcdc612bdb073326a
Murat Mirgün Ercan <muratmirgunercan225@gmail.com>
2020-11-17 23:44:03 +0300

    haxe dilinde merhaba dünya kodu eklendi

---
+++ algoritmalar/murat-m-ercan/merhaba-dünya.hx
@@ -0,0 +1,10 @@
+#* Murat Mirgün Ercan
+#* GitHub: https://github.com/MuratSs
+#* Twitter: https://twitter.com/muratmirgun
+#* Programlama Dili: Haxe
+
+class Main {
+  static public function main():Void {
+    trace("Hello World");
+  }
+}

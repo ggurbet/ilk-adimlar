4b361bd6191226f48c1c89eaf7beb98f32747d07
Gökhan Gurbetoğlu <ggurbet@gmail.com>
2025-10-04 00:03:54 +0300

    Ada programlama dilinde Merhaba Dünya

---
+++ algoritmalar/gokhan-gurbetoglu/merhaba-dunya.adb
@@ -0,0 +1,13 @@
+-- Gökhan Gurbetoğlu 
+-- GitHub: https://github.com/ggurbet
+-- LinkedIn: https://www.linkedin.com/in/ggurbet
+-- X: https://x.com/ggurbet
+-- Programlama Dili: Ada
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Merhaba_Dunya is
+begin
+   Put_Line ("Merhaba Dünya!");
+end Merhaba_Dunya;
+

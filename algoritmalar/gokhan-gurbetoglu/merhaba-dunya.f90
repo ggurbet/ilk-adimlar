bce22235728942e1b709d4cd4b14c94c9178d601
Gökhan Gurbetoğlu <ggurbet@gmail.com>
2025-10-04 00:07:06 +0300

    Fortran programlama dilinde Merhaba Dünya

---
+++ algoritmalar/gokhan-gurbetoglu/merhaba-dunya.f90
@@ -0,0 +1,10 @@
+! Gökhan Gurbetoğlu
+! GitHub: https://github.com/ggurbet
+! LinkedIn: https://www.linkedin.com/in/ggurbet
+! X: https://x.com/ggurbet
+! Programlama Dili: Fortran
+
+program MerhabaDunya
+    print *, "Merhaba Dünya!"
+end program MerhabaDunya
+

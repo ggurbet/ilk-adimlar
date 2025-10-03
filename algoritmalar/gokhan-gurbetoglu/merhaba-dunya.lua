b74a1e4bdf767bb6ba6c2e164b87717b2bf498fd
Gökhan Gurbetoğlu <ggurbet@gmail.com>
2025-10-04 00:12:23 +0300

    Lua programlama dilinde Merhaba Dünya

---
+++ algoritmalar/gokhan-gurbetoglu/merhaba-dunya.lua
@@ -0,0 +1,8 @@
+-- Gökhan Gurbetoğlu
+-- GitHub: https://github.com/ggurbet
+-- LinkedIn: https://www.linkedin.com/in/ggurbet
+-- X: https://x.com/ggurbet
+-- Programlama Dili: Lua
+
+print("Merhaba Dünya!")
+
